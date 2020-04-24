fed44b8cc6ff63f90fb849ac682a79326bc8b253
YASH <yashsp1158@gmail.com>
2020-04-24 23:19:45 +0530

    Minor changes

--- lib/Screens/CheesePage.dart
+++ lib/Screens/CheesePage.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:temp_daily_app/Screens/productDetails.dart';
+import 'productDetails.dart';
 QuerySnapshot qn, snapshot;
 String index;
 class cheeseProduct extends StatefulWidget {
